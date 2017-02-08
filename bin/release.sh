f50418e91989faabdb87d0f5d3c3ff5aafcfd8cb
Kyle Espinola <ksespinola@gmail.com>
2017-02-07 17:52:22 -0800

    add -m to commit comand. user long option flags for create realease.

--- bin/release.sh
+++ bin/release.sh
@@ -4,11 +4,11 @@ VERSION=$(manage-version update github -o ksespinola -r package-version-test -t 
 
 git add package.json
 
-git commit "[CI SKIP] version updated to $VERSION"
+git commit -m "[CI SKIP] version updated to $VERSION"
 
 git push origin master
 
-curl -H "Authorization: token $GITHUB_TOKEN" --data
+curl --header "Authorization: token $GITHUB_TOKEN" --data
 "tag_name=${VERSION}&name=Release ${VERSION}&body=${CI_PULL_REQUEST}"
 -x POST https://api.github.com/repos/ksespinola/package-version-test/releases
 
